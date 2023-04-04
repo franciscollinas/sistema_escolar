3a3575c24971f9ebf2b9ac03d8acd82463d070b6
Francisco Llinas <supervisorfocsia@gmail.com>
2023-04-04 14:56:20 -0500

    Actualizando archivo 2

---
+++ Proyecto_escuela.py
@@ -0,0 +1,24 @@
+def run():
+
+    def registro_alumnos():
+        Profesor = input("Digite su nombre profesor: ")
+        curso = input("Indique el curso a registrar: ")
+        asignatura = input("Indique la asignatura a registrar: ")
+        num_est = int(input("Indique cuantos alumnos tiene: "))
+        dicvacio ={}
+        for i in num_est:
+            for j in num_est:
+                name_est = str(input("Digite el nombre del estudiante:  "))
+                nota_est = float(input("Digite la nota del estudiante:  "))
+                
+
+
+
+
+
+
+
+
+
+if __name__=="_main__": 
+    run()
